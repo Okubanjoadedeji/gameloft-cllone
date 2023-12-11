79d26b2a55403ed2fb2cda022c8e8cde4c8e2563
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-12-11 17:14:23 +0100

    mobile responsive

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const sideMenu = document.getElementById("side-menu");
 const toggleSideMenu = document.getElementById("menu__bar");
 const closeSideMenu = document.querySelector(".side-header__burger");
 const overlayOn = document.querySelector(".side-menu__overlay");
-const togglecomDropDown = document.querySelectorAll(".com-gallery__dropdown");
+const toggleDropDown = document.querySelectorAll(".com-gallery__dropdown");
 const comDropDown = document.querySelectorAll(".com-dropdown-multi__dropdown");
 const arrowDropDown = document.querySelectorAll(".com-dropdown-multi__arrow");
 const stickyMenu = document.querySelector(".sticky-icons__group");
@@ -13,8 +13,9 @@ const toggleStickyMenu = document.querySelector(".sticky-icons__center");
 const readMore = document.querySelectorAll(".com-game-card__read-more");
 const footerElement = document.querySelector(".com-footer");
 const topButton = document.querySelector(".Button_button-top");
-const toggleStudio = document.querySelector("side-link__item");
-const studioDropdown = document.querySelector("side-link__item-group");
+const studioBtn = document.getElementById("studio-drop");
+const studioDropdown = document.querySelector(".side-link__item-group");
+const studioArrow =document.querySelector(".side-link__item-arrow");
 
 
 toggleSideMenu.addEventListener('click', ()=>{
@@ -36,8 +37,8 @@ function hideAllDropdownsExcept(clickedIndex) {
   }
 }
 
-for (let i = 0; i < togglecomDropDown.length; i++) {
-  togglecomDropDown[i].addEventListener("click", () => {
+for (let i = 0; i < toggleDropDown.length; i++) {
+  toggleDropDown[i].addEventListener("click", () => {
     const isDropdownOpen = comDropDown[i].classList.contains("show");
 
     if (isDropdownOpen) {
@@ -71,18 +72,40 @@ function showButtonOnScroll() {
 
 window.addEventListener("scroll", showButtonOnScroll);
 
-topButton.addEventListener("click", () => {
+topButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: "smooth", duration: 600 });
 });
 
+
+function hideAllDropdowns (clickedIndex) {
+    if (clickedIndex == 0) {
+      comDropDown.classList.remove("height-auto");
+      arrowDropDown.classList.remove("arrow-flip");
+    }
+  
+}
+
+
+studioBtn.addEventListener('click', () => {
+  const isDropdownOpen = studioDropdown.classList.contains("height-auto");
+
+  if (isDropdownOpen) {
+    studioDropdown.classList.remove("height-auto");
+    studioArrow.classList.remove("arrow-flip");
+  } else {
+
+    hideAllDropdowns();
+
+    studioDropdown.classList.add("height-auto");
+    studioArrow.classList.add("arrow-flip");
+  }
+});
+
+
+
+
+
 showButtonOnScroll();
-
-
-
-
-
-
-
 
 
 
